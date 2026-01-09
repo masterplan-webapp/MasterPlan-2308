3ea9e9b98258c9e489084d8e0abc803c49fc86d3
Fábio Z. <fabioz@Fabios-MacBook-Air.local>
2026-01-09 20:46:49 -0300

    fix: Update Stripe API key

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ admin.initializeApp();
 const db = admin.firestore();
 
 // Initialize Stripe with the Secret Key
-const stripe = require("stripe")("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");
+const stripe = require("stripe")("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");
 
 /**
  * Creates a Stripe Checkout Session for subscription upgrade.
