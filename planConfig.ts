f9e15a38eba784013ca36182c5ccaecc06102a53
Fabio Z <masterplan.webapp@gmail.com>
2026-01-14 19:35:11 -0300

    revert: restore Free plan maxKeywords to 50

--- planConfig.ts
+++ planConfig.ts
@@ -58,7 +58,7 @@ export const PLANS: Record<SubscriptionTier, PlanConfig> = {
             canUseCreativeBuilder: false,
             canUseAdvancedAI: false,
             maxUTMLinks: 10,
-            maxKeywords: 20,
+            maxKeywords: 50,
             maxCopyVersions: 5
         }
     },
